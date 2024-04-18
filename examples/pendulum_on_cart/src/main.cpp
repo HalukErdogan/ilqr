d114b12efef4894e33af91c8f091abaf1f08a23a
HalukErdogan <haluk_erdogan@outlook.com>
2024-04-18 14:50:22 +0300

    Improve bound check

--- examples/pendulum_on_cart/src/main.cpp
+++ examples/pendulum_on_cart/src/main.cpp
@@ -131,7 +131,7 @@ int main(int argc, char** argv) {
     file << "t,q1,q2,dq1,dq2,u" << std::endl;
 
     // Store the results
-    for (int i = 0; i < solution.states.size()-1; ++i) {
+    for (std::size_t i = 0; i + 1 < solution.states.size(); ++i) {
         // Write the current state to the file
         const auto& x = solution.states.at(i);
         const auto& u = solution.controls.at(i);
